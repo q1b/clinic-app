745ca9211314b58ba17d27d8f33361ed6a6e4f89
Sukhpreet Singh <peadevp@gmail.com>
2023-09-24 01:30:18 +0530

    made the session object available all over the server

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,6 +3,9 @@ import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
   // we can pass `event` because we used the SvelteKit middleware
-  event.locals.auth = auth.handleRequest(event);
+  const lucia = auth.handleRequest(event);
+  event.locals.auth = lucia;
+  event.locals.session = await lucia.validate();
+  event.locals.user = event.locals.session?.user ?? null;
   return await resolve(event);
 };
