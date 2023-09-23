519bcbfa14fc08626e05889dc32c3d10ab54fcc5
Sukhpreet Singh <peadevp@gmail.com>
2023-09-24 01:33:11 +0530

    removed unused code

--- src/lib/server/kv.ts
+++ src/lib/server/kv.ts
@@ -1,19 +1,9 @@
 import { Redis } from "@upstash/redis";
 import * as env from "$env/static/private";
-import { dev } from "$app/environment";
-
-declare global {
-	// eslint-disable-next-line no-var
-	var __upstash_kv: import('@upstash/redis').Redis;
-}
 
 const upstashClient = new Redis({
   url: env.UPSTASH_URL,
   token: env.UPSTASH_TOKEN
 });
-
-if (dev) {
-	globalThis.__upstash_kv = upstashClient;
-}
 
 export default upstashClient;
