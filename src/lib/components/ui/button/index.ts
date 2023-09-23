dd44974ff4320069b51582a1168015f8c7e1b24b
Sukhpreet Singh <peadevp@gmail.com>
2023-09-24 01:31:34 +0530

    made it super simple for now

--- src/lib/components/ui/button/index.ts
+++ src/lib/components/ui/button/index.ts
@@ -3,24 +3,20 @@ import { tv, type VariantProps } from "tailwind-variants";
 import Root from "./button.svelte";
 
 const buttonVariants = tv({
-	base: "inline-flex items-center justify-center rounded-md text-sm font-medium whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+	base: `inline-flex items-center justify-center whitespace-nowrap
+				ring-offset-slate-100
+				focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-500 focus-visible:ring-offset-2
+				disabled:pointer-events-none disabled:opacity-50
+				[-webkit-tap-highlight-color:transparent;] select-none
+				border 
+	`,
 	variants: {
 		variant: {
 			default:
-				"bg-primary text-primary-foreground shadow hover:bg-primary/90",
-			destructive:
-				"bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
-			outline:
-				"border border-input bg-transparent shadow-sm hover:bg-accent hover:text-accent-foreground",
-			secondary:
-				"bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
-			ghost: "hover:bg-accent hover:text-accent-foreground",
-			link: "text-primary underline-offset-4 hover:underline"
+				"bg-white text-slate-900 border-slate-600",
 		},
 		size: {
-			default: "h-9 px-4 py-2",
-			sm: "h-8 rounded-md px-3 text-xs",
-			lg: "h-10 rounded-md px-8",
+			default: "h-9 px-3 py-1 gap-x-2",
 			icon: "h-9 w-9"
 		}
 	},
