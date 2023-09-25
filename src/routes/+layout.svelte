99c8bf6b3b60f2585bf27438e3e09ddc57d1b78a
Sukhpreet Singh <peadevp@gmail.com>
2023-09-25 15:16:41 +0530

    Home Back Button Added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	import '../app.postcss';
-	import { LogOutIcon } from 'lucide-svelte';
+	import { HomeIcon, LogOutIcon } from 'lucide-svelte';
 	import Logo from '$lib/components/ui/logo.svelte';
 	import Google from '$lib/components/ui/icons/google.svelte';
 	import Button from '$lib/components/ui/button/button.svelte';
 	import { enhance } from '$app/forms';
 	import { Loader2Icon } from 'lucide-svelte';
 	import type { LayoutData } from './$types';
+	import { page } from '$app/stores';
+	import { goto } from '$app/navigation';
 	let loading = false;
 	export let data: LayoutData;
 </script>
@@ -17,12 +19,25 @@
 
 <header class="w-full max-w-sm bg-white px-3 py-2 flex justify-between items-center">
 	<div id="logo" class="flex items-center gap-x-2 text-slate-700">
-		{#if data.user?.image}
-			<a href={`/students/student/${data.user.id}`}>
-				<img width={48} height={48} src={data.user.image} aria-hidden="true" alt="Avatar Picture" />
-			</a>
+		{#if $page.url.pathname === '/'}
+			{#if data.user?.image}
+				<a href={`/students/student/${data.user.id}`}>
+					<img
+						width={48}
+						height={48}
+						src={data.user.image}
+						aria-hidden="true"
+						alt="Avatar Picture"
+					/>
+				</a>
+			{:else}
+				<Logo size={48} />
+			{/if}
 		{:else}
-			<Logo size={48} />
+			<Button on:click={() => goto('/')} disabled={loading} type="submit" class="text-base">
+				<HomeIcon />
+				Home
+			</Button>
 		{/if}
 	</div>
 	<nav>
