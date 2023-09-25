8b037f4ebf5eb2341bcfa1202105b73e455e23f9
Sukhpreet Singh <peadevp@gmail.com>
2023-09-26 02:14:00 +0530

    formatting

--- src/lib/components/ui/button/button.svelte
+++ src/lib/components/ui/button/button.svelte
@@ -1,15 +1,22 @@
 <script lang="ts">
-	import { Button as ButtonPrimitive } from "bits-ui";
-	import { cn } from "$lib/utils";
-	import { buttonVariants, type Props, type Events } from ".";
+	import { Button as ButtonPrimitive } from 'bits-ui';
+	import { cn } from '$lib/utils';
+	import { buttonVariants, type Props, type Events } from '.';
 
 	type $$Props = Props;
 	type $$Events = Events;
 
-	let className: $$Props["class"] = undefined;
-	export let variant: $$Props["variant"] = "default";
-	export let size: $$Props["size"] = "default";
-	export let builders: $$Props["builders"] = [];
+	let className: $$Props['class'] = undefined;
+	/**
+	 * Is this the principal call to action on the page?
+	 */
+	export let variant: $$Props['variant'] = 'default';
+	/**
+	 * How large should the button be?
+	 */
+	export let size: $$Props['size'] = 'default';
+
+	export let builders: $$Props['builders'] = [];
 	export { className as class };
 </script>
 
--- src/lib/components/ui/button/index.ts
+++ src/lib/components/ui/button/index.ts
@@ -3,17 +3,15 @@ import { tv, type VariantProps } from "tailwind-variants";
 import Root from "./button.svelte";
 
 const buttonVariants = tv({
-	base: `inline-flex items-center justify-center whitespace-nowrap
-				ring-offset-slate-100
-				focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-500 focus-visible:ring-offset-2
-				disabled:pointer-events-none disabled:opacity-50
-				[-webkit-tap-highlight-color:transparent;] select-none
-				border 
+	base: `
+				inline-flex items-center justify-center 
+				whitespace-nowrap [-webkit-tap-highlight-color:transparent;] select-none
+				ring-offset-slate-100 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-500 focus-visible:ring-offset-2
+				disabled:pointer-events-none disabled:opacity-50 
 	`,
 	variants: {
 		variant: {
-			default:
-				"bg-white text-slate-900 border-slate-600",
+			default: "bg-white text-slate-900 border border-slate-600",
 		},
 		size: {
 			default: "h-9 px-3 py-1 gap-x-2",
